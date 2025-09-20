04a79eee7e46c7a8f3c91615737e719ef5200605
HandsomeBisu <joscar12@daum.net>
2025-09-20 18:36:38 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,2 @@
-
-// DPS VALORANT CHAMPIONS - index.js
-// 현재 페이지는 정적 컨텐츠만 표시하므로, 이 파일은 비어있습니다.
-console.log("DPS Valorant Champions - Home page loaded.");
+console.log("남정환 바보")
+console.log("이 메시지를 발견하셨나요? 축하합니다. 숨겨진 메시지를 찾으셨군요! 선물은 없습니다")
